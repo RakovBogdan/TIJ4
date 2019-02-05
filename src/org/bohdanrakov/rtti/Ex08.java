8588ca655f5cbce4dee673aa00d96eefad48c363
Rakov, Bohdan <rakovb01@kits.kfplc.com>
2019-02-05 18:53:13 +0200

    Ex 09 nearly implemented

--- src/org/bohdanrakov/rtti/Ex08.java
+++ src/org/bohdanrakov/rtti/Ex08.java
@@ -1,13 +1,18 @@
 package org.bohdanrakov.rtti;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Ex08 {
+
+    private static HashSet<Class> displayedClasses = new HashSet<>();
+
 
     public static void main(String[] args) throws Exception {
         HashMap<String, String> test = new HashMap<>();
         FancyToy fancyToy = new FancyToy();
-        printClassHierarchy(fancyToy);
+        printClassHierarchy(test);
     }
 
     private static void printClassHierarchy(Object object) {
@@ -16,6 +21,8 @@ public class Ex08 {
     }
 
     private static void printClassesRecursively(Class clazz, int hierarchyPlace) {
+        displayedClasses.add(clazz);
+
         if (clazz == null) {
             return;
         }
@@ -26,6 +33,17 @@ public class Ex08 {
         }
 
         System.out.println(stringBuilder.toString() + clazz.getName());
+
+        Field[] declaredFields = clazz.getDeclaredFields();
+        if (declaredFields.length != 0) {
+            System.out.println(clazz.getName() + " fields:");
+            for (Field field : declaredFields) {
+                if (!displayedClasses.contains(field.getDeclaringClass())) {
+                    printClassesRecursively(field.getDeclaringClass(), hierarchyPlace + 1);
+                    displayedClasses.add(field.getDeclaringClass());
+                }
+            }
+        }
 
         Class superClass = clazz.getSuperclass();
         for (Class anInterface : clazz.getInterfaces()) {
